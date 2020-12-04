3b3c53c80c9e29e6e8f537bec0ab9fc5cc3733fc
udidgood1 <72788100+udidgood1@users.noreply.github.com>
2020-12-04 20:57:46 +0800

    SiyuChen

---
+++ classproject/js/calculate.js
@@ -0,0 +1,58 @@
+var names = [];
+var scores = [];
+
+window.onload = function () {
+	$("add").onclick = addScore;
+	$("display_results").onclick = displayResults;
+	$("display_scores").onclick = displayScores;
+}
+
+var $ = function (id) { return document.getElementById(id); }
+
+var addScore = function(){
+    var name = $("name").value;
+    var score = $("score").value;
+
+    names[names.length] = name;
+    scores[scores.length] = parseInt(score);
+}
+
+var displayResults = function(){
+    var sum = 0;
+    var highest_score;
+    for(var i=0;i<scores.length;i++){
+        sum = sum + scores[i];
+        var average = parseFloat(sum) / parseFloat(scores.length);
+        if(average > 90 && average<100){
+            highest_score = "A";
+            
+        }
+        else if(average > 80 && average < 89){
+            highest_score = "B";
+            
+        }
+        else if(average > 69 && average < 79){
+            highest_score = "C";
+            
+        }
+        else if(average > 60 && average < 69){
+            highest_score = "D";
+            
+        }
+        else{
+            highest_score = "F";
+            
+        }
+    }    
+    $("results").innerHTML = "<h2>Results</h2> <p> Average score = "+average+"</p><p>GPA = with a score of "+highest_score+"</p>";
+    
+}
+
+var displayScores = function(){
+    var html = "<h2>Scores</h2><tr><td><b>Name</b></td><td><b>Score</b></td></tr>"
+    for(var i=0;i<scores.length;i++){
+        html = html + "<tr><td>"+names[i]+"</td><td>"+scores[i]+"</td></tr>";
+        
+    }
+    $("scores_table").innerHTML = html;
+}
